93d669cb17cfdd271e23a4541d2ba6c495feaca8
Dasun169 <dasunnavindu2001@gmail.com>
2025-04-09 22:49:36 +0530

    create UserRoute.js route file to handle CRUD functions routes

--- backend/Routes/UserRoute.js
+++ backend/Routes/UserRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../Model/UserModel");
 const UserController = require("../Controllers/UserController");
 
+router.post("/", UserController.addUsers);
 router.get("/", UserController.getAllUsers);
+router.get("/email/:email", UserController.getUserByEmail);
+router.get("/id-by-email/:email", UserController.getUserIdByEmail);
+router.put("/update/:id", UserController.updateUserById);
 
 module.exports = router;
