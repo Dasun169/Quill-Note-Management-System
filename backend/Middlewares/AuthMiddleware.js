782167452830da65b91cd9ef5516dddd86c557e6
Dasun169 <dasunnavindu2001@gmail.com>
2025-04-23 11:49:59 +0530

    create verify token method

--- backend/Middlewares/AuthMiddleware.js
+++ backend/Middlewares/AuthMiddleware.js
@@ -2,6 +2,30 @@ const User = require("../Model/UserModel");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
+module.exports.verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ status: false, message: "No token provided" });
+  }
+
+  jwt.verify(token, process.env.TOKEN_KEY, async (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ status: false, message: "Invalid token" });
+    }
+
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json({ status: false, message: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  });
+};
+
 module.exports.userVerification = (req, res) => {
   const token = req.cookies.token;
 
@@ -14,8 +38,17 @@ module.exports.userVerification = (req, res) => {
       return res.json({ status: false });
     } else {
       const user = await User.findById(data.id);
-      if (user) return res.json({ status: true, user: user.username });
-      else return res.json({ status: false });
+      if (user) {
+        return res.json({
+          status: true,
+          user: {
+            email: user.email,
+            name: user.name,
+          },
+        });
+      } else {
+        return res.json({ status: false });
+      }
     }
   });
 };
