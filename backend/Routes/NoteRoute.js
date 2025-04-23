c55b8a1ac3e34ca981617dcfde1e8cad98124853
Dasun169 <dasunnavindu2001@gmail.com>
2025-04-23 19:36:53 +0530

    Update the NoteRoute.js file to get all Notes according to the email

--- backend/Routes/NoteRoute.js
+++ backend/Routes/NoteRoute.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const NoteController = require("../Controllers/NoteController");
 
 router.post("/create", NoteController.createNote);
+router.get("/all", NoteController.getAllNotes);
+router.get("/notes/:email", NoteController.getNotesByEmail);
 router.put("/update/:email/:title", NoteController.updateNoteByTitleAndEmail);
 router.delete(
   "/delete/:email/:title",
